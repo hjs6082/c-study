dc135e026aab2fb015ab65f82792b1ed602c0468
DESKTOP-77DPHAE\ROG <hjs77143@gmail.com>
2021-04-08 19:08:12 +0900

    피보나치

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int Fibonachi(int n) {
+	if (n == 1 || n == 2)
+		return 1;
+	else
+		return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+
+int main()
+{
+	int i;
+	for (i = 1; i <= 15; i++)
+		cout << Fibonachi(i) << endl;
+}
