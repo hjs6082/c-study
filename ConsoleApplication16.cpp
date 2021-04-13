6be6f6399a62cffd045ad547c35cbfaa59ab3a7d
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2021-04-13 19:11:59 +0900

    입력시 구구단 출력

---
+++ ConsoleApplication16.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int Bonus2(int a, int b)
+{
+	if (a <= 9)
+	{
+		cout << a << "*" << b << "=" << a * b << endl;
+		if (b < 9)
+		{
+			Bonus2(a, b + 1);
+		}
+		else
+		{
+			return Bonus2(a + 1, 1);
+		}
+	}
+
+}
+
+int main()
+{
+	int i = 2, j = 1;
+	Bonus2(i, j);
+
+}
