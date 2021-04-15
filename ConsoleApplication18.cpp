ceec7a1ed3c95b2b72d1a6c88783de21a78fb85c
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2021-04-15 15:58:29 +0900

    각 자리수의 합

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int s = 0;
+
+int Bonus1(int n)
+{
+	int a = 0;
+	if (n < 10)
+	{
+		a = n;
+		s += n;
+		return s;
+	}
+	else
+	{
+		a = n % 10;
+		s += n;
+		return Bonus1(n / 10);
+	}
+
+}
+
+int main()
+{
+	int d;
+	cin >> d;
+	cout << Bonus1(d) << endl;
+}
